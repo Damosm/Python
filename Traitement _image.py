444c7e3df0fae1026ac797d4efa034f35319aa5a
damosm <damosm@free.fr>
2020-02-03 09:48:17 +0100

    second

--- Traitement _image.py
+++ Traitement _image.py
@@ -2,7 +2,7 @@ import matplotlib.image as mpimg
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-
+"""
 rouge=[1,0,0]
 cyan=[0,1,1]
 jaune=[1,1,0]
@@ -88,6 +88,64 @@ def separation (img) :
     #img_bleu[:,:,2]=img[:,:,2]
 
     return img_rouge
+"""
+#########################################################################
+a = pl.imread('c:/Users/ThinkCentre/Documents/Python/Coline.PNG')
+#print(a.shape)
+b=a
+
+#b=a[:,:,0]
+
+
+rouge=[1,0,0]
+vert= [0,1,0]
+bleu= [0,0,1]
+
+c= np.zeros((10,10,3))
+c[:,:]=rouge
+"""
+def crea(c):
+    rouge=[1,0,0]
+    
+    #im = np.copy(img_orig) # On fait une copie de l'original
+    for i in enumerate(c):
+        for j in enumerate(i):
+            #r, v, b = im[i, j]
+            #im[i, j] = (r, 0,0)
+            #c[i,j]=rouge
+            #print(j)
+            for k in enumerate(j) :
+                print(k)
+ 
+    return c
+"""
+
+def transform (a) :
+    
+    c= np.copy(a)
+
+    for i in range(c.shape[0]) :
+        for j in range(c.shape[1]) :
+
+            #print(c[i,j])
+            if np.array_equal(c[i,j], rouge) == True :
+                c[i,j]=bleu           
+        
+    
+    return c
+
+def rgb2gray(img):
+    return np.dot(img[...,:3], [0.299, 0.587, 0.144])
+
+#c= transform(c)
+#c.getpixel((0,0))[0]
+c= rgb2gray(a)
+"""
+print(b)
+mpimg.imsave('c:/Users/ThinkCentre/Documents/Python/Coline_modif.PNG',b)
+"""
+pl.imshow(c)
+pl.show()
                 
             
 
